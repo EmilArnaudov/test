fd672470c13e8b1f00d9674d0b2923c28275ed09
Emil <emil.arnaudov.ecom@gmail.com>
2024-07-25 22:46:37 +0300

    add drawer nav

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import "./style.css";
 
 const burgerMenu = document.getElementById("burgerMenu")!;
+const drawer = document.getElementById("drawer")!;
 burgerMenu.addEventListener("click", function () {
   this.classList.toggle("active");
+  drawer.classList.toggle("open");
+});
+
+document.addEventListener("click", (event: MouseEvent) => {
+  const target = event.target as HTMLElement;
+  if (!drawer.contains(target) && target !== burgerMenu) {
+    drawer.classList.remove("open");
+    burgerMenu.classList.remove("active");
+  }
 });
