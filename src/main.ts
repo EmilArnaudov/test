284dbb8e1ebe176d4a488f83165718d440004ade
Emil <emil.arnaudov.ecom@gmail.com>
2024-07-26 16:36:59 +0300

    add scroll to section

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,48 @@ import "./style.css";
 
 const burgerMenu = document.getElementById("burgerMenu")!;
 const drawer = document.getElementById("drawer")!;
-burgerMenu.addEventListener("click", function () {
-  this.classList.toggle("active");
-  drawer.classList.toggle("open");
-});
+const booster = document.getElementById("booster")!;
+const protein = document.getElementById("protein")!;
+
+burgerMenu.addEventListener("click", toggleDrawer);
 
 document.addEventListener("click", (event: MouseEvent) => {
   const target = event.target as HTMLElement;
+
   if (!drawer.contains(target) && target !== burgerMenu) {
-    drawer.classList.remove("open");
-    burgerMenu.classList.remove("active");
+    closeDrawer();
   }
 });
+
+drawer.addEventListener("click", (event: MouseEvent) => {
+  const target = event.target as HTMLElement;
+  console.log(target.dataset.id);
+  if (!target.dataset.id) return;
+
+  closeDrawer();
+  switch (target.dataset.id) {
+    case "booster":
+      booster.scrollIntoView({ behavior: "smooth" });
+      break;
+    case "protein":
+      protein.scrollIntoView({ behavior: "smooth" });
+      break;
+    default:
+      break;
+  }
+});
+
+function openDrawer(): void {
+  drawer.classList.add("open");
+  burgerMenu.classList.add("active");
+}
+
+function closeDrawer(): void {
+  drawer.classList.remove("open");
+  burgerMenu.classList.remove("active");
+}
+
+function toggleDrawer(): void {
+  drawer.classList.toggle("open");
+  burgerMenu.classList.toggle("active");
+}
